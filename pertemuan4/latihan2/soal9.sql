d3e6198f42e067470d17fd1eac50a977030c46aa
silma <145253957+silmatsania@users.noreply.github.com>
2024-10-24 16:08:08 +0700

    commit

--- pertemuan4/latihan2/soal9.sql
+++ pertemuan4/latihan2/soal9.sql
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-
-
 select * from jadwal_mata_kuliah
 select nama_mata_kuliah
 from jadwal_mata_kuliah
 where kode_mata_kuliah IN ('TI101', 'SI201', 'TS301')
-=======
-select * from jadwal_mata_kuliah
-select nama_mata_kuliah
-from jadwal_mata_kuliah
-where kode_mata_kuliah IN ('TI101', 'SI201', 'TS301')
->>>>>>> 42c984b6e4b80fa18d14362c60b5e2c7468be402
