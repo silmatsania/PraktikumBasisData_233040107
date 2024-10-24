dbf5fbee0797954792dd3746f6bdc9fe3c91850b
silma <145253957+silmatsania@users.noreply.github.com>
2024-10-24 16:12:30 +0700

    commit

--- pertemuan4/latihan2/soalno7.sql
+++ pertemuan4/latihan2/soalno7.sql
@@ -1,3 +1,6 @@
 SELECT * FROM dbo.jadwal_mata_kuliah 
-WHERE dosen_pengajar like '%ang%' 
+WHERE dosen_pengajar like '%ang%'select * from jadwal_mata_kuliah
+select nama_mata_kuliah
+from jadwal_mata_kuliah
+where kode_mata_kuliah IN ('TI101', 'SI201', 'TS301')
 ORDER BY nama_mata_kuliah
