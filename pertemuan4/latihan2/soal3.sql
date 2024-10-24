93c64593f9239ae3c366317e613bda1e7a7673a1
silma <145253957+silmatsania@users.noreply.github.com>
2024-10-24 16:05:45 +0700

    commit

--- pertemuan4/latihan2/soal3.sql
+++ pertemuan4/latihan2/soal3.sql
@@ -1,5 +1,4 @@
 select count(*) as jumlah_total_mahasiswa_informatika
 from mahasiswa 
 where jurusan = 'Teknik Informatika'
-where jurusan = 'Teknik Informatika'
->>>>>>> 42c984b6e4b80fa18d14362c60b5e2c7468be402
+
