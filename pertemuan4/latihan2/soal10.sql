97e17205ab2bdae68c00d271039db508dca1a0f2
silma <145253957+silmatsania@users.noreply.github.com>
2024-10-24 16:09:37 +0700

    commit

--- pertemuan4/latihan2/soal10.sql
+++ pertemuan4/latihan2/soal10.sql
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 SELECT nama_mahasiswa 
 FROM mahasiswa 
 WHERE mahasiswa_id   IN (
@@ -7,14 +5,3 @@ SELECT mahasiswa_id
 FROM dpp_mahasiswa
 WHERE status_pembayaran = 'Lunas'
 );
-
-=======
-SELECT nama_mahasiswa 
-FROM mahasiswa 
-WHERE mahasiswa_id   IN (
-SELECT mahasiswa_id  
-FROM dpp_mahasiswa
-WHERE status_pembayaran = 'Lunas'
-);
-
->>>>>>> 42c984b6e4b80fa18d14362c60b5e2c7468be402
