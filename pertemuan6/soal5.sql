f27d9edb8ff685ee9c80047bb3c6c8ba892e6781
silma <145253957+silmatsania@users.noreply.github.com>
2024-10-31 16:35:51 +0700

    first commit

--- pertemuan6/soal5.sql
+++ pertemuan6/soal5.sql
@@ -1,9 +1,3 @@
-SELECT 
-    Employee.Name AS KaryawanNama, 
-    Projects.ProjectName AS ProyekNama
-FROM 
-    Employee
-CROSS JOIN 
-    Projects
-ORDER BY 
-    Employee.Name, Projects.ProjectName;
+SELECT Employee.Name, Projects.ProjectName
+FROM Employee
+CROSS JOIN Projects;
